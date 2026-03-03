85d1b5cc25a9ffdbd5b2f546f8deb4fa306fa958
angelhdz101 <angel.uriel@itsvc.edu.mx>
2026-03-03 16:45:00 -0600

    cambios2 3/3/2026

--- src/app/acciones/Inventario/inventario.component.ts
+++ src/app/acciones/Inventario/inventario.component.ts
@@ -1,11 +1,14 @@
 import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatTableModule, MatTableDataSource } from '@angular/material/table';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
 
-export interface Inventario {
+@Component({
+  selector: 'app-inventario',
+  standalone: true,
+  templateUrl: './inventario.component.html',
+  styleUrls: ['./inventario.component.css']
+})
+export class InventarioComponent {
+
+ 
 
 }
